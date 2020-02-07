76ef57c0960011158107c2415f6278c4771abc37
Priscilla Presto <49257688+priscilladeltoro10@users.noreply.github.com>
2020-02-06 16:29:58 -0800

    adjusted content

--- wordpress/wp-content/themes/wordpress-theme-starter-master/page-home.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/page-home.php
@@ -8,69 +8,49 @@ get_header(); ?>
 	<main role="main">
 		<section>
             <div class="banner container-fluid">
-                <img src="http://webcrumbs.site/wp-content/uploads/2020/02/WebCrumbs-Group-Photo-JPEG-scaled.jpg">
+                <img src="http://peskyrealm.site/wp-content/uploads/2020/02/still-life-composition.png">
             </div>
         </section>
         <section>
-            <div class="box">
-                <div class="grid">
-                    <div class="grid-temp">
-                        <div class="item1">
-                            <picture class="picback">
-                                <img src="http://webcrumbs.site/wp-content/uploads/2020/02/webcrumbs-left-quotation-01.png">
-                            </picture>
-                        </div>
-                        <div class="quotation item2">
-                            <p>At WebCrumbs, we always make sure to have our HAT on...</p>
-                        </div>
-                        <div class="item3">
-                            <picture class="picback">
-                                <img src="http://webcrumbs.site/wp-content/uploads/2020/02/webcrumbs-right-quotation-01-01.png">
-                            </picture>
-                        </div>
-                    </div>          
-                </div>
-            </div>      
+            <div class="space"></div>
         </section>
         <section>
             <div id="crumbs" class="carousel slide container-fluid" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#crumbs" data-slide-to="0" class="active"><img class="img-responsive" src="http://webcrumbs.site/wp-content/uploads/2020/02/webcrumbs-single-crumb-one-01.png" alt="crumbs-indicator">
-                </li>
-                <li data-target="#crumbs" data-slide-to="1"><img class="img-responsive" src="http://webcrumbs.site/wp-content/uploads/2020/02/webcrumbs-single-crumb-two-01.png" alt="crumbs-indicator">
-                </li>
-                <li data-target="#crumbs" data-slide-to="2"><img class="img-responsive" src="http://webcrumbs.site/wp-content/uploads/2020/02/webcrumbs-single-crumb-three-01.png" alt="crumbs-indicator">
-                </li>
-            </ol>
+                <ol class="carousel-indicators">
+                    <li data-target="#crumbs" data-slide-to="0" class="active"></li>
+                    <li data-target="#crumbs" data-slide-to="1"></li>
+                    <li data-target="#crumbs" data-slide-to="2"></li>
+                </ol>
 				<div class="carousel-inner">
 					<div class="carousel-item active">
+                        <picture>
+                            <img src="http://peskyrealm.site/wp-content/uploads/2020/02/angler-fish-bottom-flat-view-scaled-e1581034651946.jpg" alt="angler fish under deck">
+                        </picture>
                         <div class="carousel-caption">
-                            <h1>H <a style="opacity: 30%"> A T</a></h1>
-                            <h2>Helpful</h2>
-                            <h3>We are a click, text, call, drive <br> away, and always ready to help.</h3>
+                            <h1>Custom</h1>
+                            <h2>You choose your own style, <br> from top to bottom.</h2>
                         </div> 
   					</div>
-  					<div class="carousel-item">
+  					<div class="carousel-item" >
+                        <picture>
+                            <img src="http://peskyrealm.site/wp-content/uploads/2020/02/aluminum-green-frontal-side-view-scaled-e1581035062380.jpg" alt="aluminum deck">
+                        </picture>
                         <div class="carousel-caption">
-                            <h1><a style="opacity: 30%">H</a> A <a style="opacity: 30%">T</a></h1>
-                            <h2>Authentic</h2>
-                            <h3>We work to showcase the best, <br> unique you in the digital world.</h3>
+                            <h1>Local</h1>
+                            <h2>All artwork is commissioned <br> from local talent.</h2>
                         </div> 
   					</div>
 					<div class="carousel-item">
+                        <picture>
+                            <img src="http://peskyrealm.site/wp-content/uploads/2020/02/angler-fish-bottom-flat-view-scaled-e1581034651946.jpg" alt="angler fish under deck">
+                        </picture>
                         <div class="carousel-caption">
-                            <h1><a style="opacity: 30%">H A</a> T</h1>
-                            <h2>Transparent</h2>
-                            <h3>We believe in a relationship <br> with honest communication.</h3>
+                            <h1>Women</h1>
+                            <h2>We are founded by women, <br> and encourage all to ride.</h2>
                         </div> 
   					</div>
 				</div>
 			</div>
-        </section>
-        <section>
-            <div class="button-container container-fluid">
-                <a class="button" href="https://square.site/book/GDH2KVERNEFSC/webcrumbs-fresno-ca" data-toggle="tooltip" title="Hooray!"> Book A Meeting</a>
-            </div>
         </section>
 
 	</main>
