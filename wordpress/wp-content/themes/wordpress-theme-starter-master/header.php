f9501cae1392a0340e189c2081ff2e39a79ffd2c
Priscilla Presto <49257688+priscilladeltoro10@users.noreply.github.com>
2020-02-10 10:56:25 -0800

    added navwalker and portfolio images

--- wordpress/wp-content/themes/wordpress-theme-starter-master/header.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/header.php
@@ -34,5 +34,30 @@
 
 			<!-- header -->
 			<header class="header clear" role="banner">
+				<nav class="navbar navbar-default" role="navigation"> 
+					<!-- Brand and toggle get grouped for better mobile display --> 
+					<div class="navbar-header"> 
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse"> 
+							<span class="sr-only">Toggle navigation</span> 
+							<span class="icon-bar"></span> 
+							<span class="icon-bar"></span> 
+							<span class="icon-bar"></span> 
+						</button> 
+						<a class="navbar-brand" href="<?php bloginfo('url')?>"><?php bloginfo('name')?></a> 
+					</div> 
+					<!-- Collect the nav links, forms, and other content for toggling --> 
+					<div class="collapse navbar-collapse navbar-ex1-collapse"> 
+					<?php /* Primary navigation */
+					wp_nav_menu( array(
+						'menu' => 'top_menu',
+						'depth' => 2,
+						'container' => false,
+						'menu_class' => 'nav',
+						//Process nav menu using our custom nav walker
+						'walker' => new wp_bootstrap_navwalker())
+					);
+					?>
+					</div>
+				</nav>
 			</header>
 			<!-- /header -->
