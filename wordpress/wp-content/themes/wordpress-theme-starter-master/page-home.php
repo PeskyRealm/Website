a9feef8e5257a87cbb5c042984043f4fba152b2e
Priscilla Presto <49257688+priscilladeltoro10@users.noreply.github.com>
2020-02-10 16:53:41 -0800

    made carousel vertical

--- wordpress/wp-content/themes/wordpress-theme-starter-master/page-home.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/page-home.php
@@ -15,7 +15,7 @@ get_header(); ?>
             <div class="space"></div>
         </section>
         <section>
-            <div id="crumbs" class="carousel slide container-fluid" data-ride="carousel">
+            <div id="crumbs" class="carousel slide container-fluid carousel.vertical" data-ride="carousel">
                 <ol class="carousel-indicators">
                     <li data-target="#crumbs" data-slide-to="0" class="active"></li>
                     <li data-target="#crumbs" data-slide-to="1"></li>
@@ -26,28 +26,28 @@ get_header(); ?>
                         <picture>
                             <img src="http://peskyrealm.site/wp-content/uploads/2020/02/angler-fish-bottom-flat-view-scaled-e1581034651946.jpg" alt="angler fish under deck">
                         </picture>
-                        <div class="carousel-caption">
+                        <!-- <div class="carousel-caption">
                             <h1>Custom</h1>
                             <h2>You choose your own style, <br> from top to bottom.</h2>
-                        </div> 
+                        </div>  -->
   					</div>
   					<div class="carousel-item" >
                         <picture>
                             <img src="http://peskyrealm.site/wp-content/uploads/2020/02/aluminum-green-frontal-side-view-scaled-e1581035062380.jpg" alt="aluminum deck">
                         </picture>
-                        <div class="carousel-caption">
+                        <!-- <div class="carousel-caption">
                             <h1>Local</h1>
                             <h2>All artwork is commissioned <br> from local talent.</h2>
-                        </div> 
+                        </div>  -->
   					</div>
 					<div class="carousel-item">
                         <picture>
                             <img src="http://peskyrealm.site/wp-content/uploads/2020/02/angler-fish-bottom-flat-view-scaled-e1581034651946.jpg" alt="angler fish under deck">
                         </picture>
-                        <div class="carousel-caption">
+                        <!-- <div class="carousel-caption">
                             <h1>Women</h1>
                             <h2>We are founded by women, <br> and encourage all to ride.</h2>
-                        </div> 
+                        </div>  -->
   					</div>
 				</div>
 			</div>
